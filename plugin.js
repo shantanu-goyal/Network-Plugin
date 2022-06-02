14ab583e2e67724adda7e251136615e01eb262c2
Shantanu Goyal <shantanu.goyal@sprinklr.com>
2022-06-02 22:40:26 +0530

    Added Extension

---
+++ plugin.js
@@ -0,0 +1,10 @@
+module.exports = {
+  audits: [{
+    path: 'lighthouse-plugin-network/audits/network.js'
+  }],
+  category: {
+    title: "Network",
+    description: "Logs all network request",
+    auditRefs: [{ id: 'third-party-summary', weight: 1 }]
+  }
+};
